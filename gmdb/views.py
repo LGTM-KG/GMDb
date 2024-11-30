81e1cb8728decfc2293b613f762be7445afcbbed
trisnobp <trisnotyo6@gmail.com>
2024-11-30 14:18:31 +0700

    feat: home page ui

--- gmdb/views.py
+++ gmdb/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rdflib import Graph
+
+local_g = Graph()
+local_g.parse('Integrated_Movies_Triples.ttl')
 
 # Create your views here.
 def home_page(request):
