df161374120e44205bf5c8b71c2aac3c5f163562
Hans TM <112265332+HansTM@users.noreply.github.com>
2024-12-05 20:52:48 +0700

    Lengkapi halaman detail film

--- gmdb/views.py
+++ gmdb/views.py
@@ -4,6 +4,7 @@ from rdflib.plugins.sparql import prepareQuery
 import re
 import datetime
 import requests
+import humanize
 
 local_g = rdflib.Graph()
 local_g.parse('Integrated_Movies_Triples.ttl')
@@ -115,24 +116,38 @@ def search_movies(request):
 
     return render(request, "search.html", context)
 
-DETAIL_Q = prepareQuery(
-    INITIAL_NAMESPACES + """ 
+DETAIL_NAMESPACES = INITIAL_NAMESPACES + """
     PREFIX wikibase: <http://wikiba.se/ontology#>
     PREFIX bd: <http://www.bigdata.com/rdf#>
     PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+    PREFIX p: <http://www.wikidata.org/prop/>
+    PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+    PREFIX ps: <http://www.wikidata.org/prop/statement/>
+"""
 
+
+DETAIL_Q = prepareQuery(
+    DETAIL_NAMESPACES + """ 
     SELECT * WHERE {
-		?s rdfs:label ?label.
+		?s rdfs:label ?label .
 		?s v:hasWikidata ?item .
         OPTIONAL { ?s v:poster ?poster . }
         OPTIONAL { ?s v:genre ?genre . }
-        OPTIONAL { ?s v:worldWideSales ?worldWideSales . }
         OPTIONAL { ?s v:releaseDate ?releaseDate . }
         OPTIONAL { ?s v:runtime ?runtime . }
         OPTIONAL { ?s v:releasedYear ?releasedYear . }
         OPTIONAL { ?s v:releasedDate ?releasedDate . }
+        OPTIONAL { ?s v:budget ?budget . }
         OPTIONAL { ?s v:certification ?certification . }
+        OPTIONAL { ?s v:domesticOpening ?domesticOpening . }
+        OPTIONAL { ?s v:domesticSales ?domesticSales . }
+        OPTIONAL { ?s v:internationalSales ?internationalSales . }
+        OPTIONAL { ?s v:worldWideSales ?worldWideSales . }
+        OPTIONAL {
+            ?s v:metaScore ?metaScore .
+        }
         OPTIONAL { ?s v:overview ?overview . }
+
 		SERVICE <https://query.wikidata.org/sparql> {
 			?item rdfs:label ?itemLabel .
 			FILTER(LANG(?itemLabel) = "en")
@@ -160,6 +175,104 @@ DETAIL_Q = prepareQuery(
 		}
 	}"""
 )
+        
+DETAIL_IMDB_RATING_Q = prepareQuery(
+    DETAIL_NAMESPACES + """
+    SELECT ?imdbRating ?imdbVotes WHERE {
+        ?s v:imdbScore ?imdbScore .
+        ?imdbScore v:rating ?imdbRating .
+        ?imdbScore v:numOfVotes ?imdbVotes .    
+    }
+    """
+)
+
+DETAIL_PRODUCER_Q = prepareQuery(
+    DETAIL_NAMESPACES + """ 
+    SELECT * WHERE {
+        ?s v:hasWikidata ?item .
+        SERVICE <https://query.wikidata.org/sparql> {
+            OPTIONAL {
+                ?item wdt:P162 ?producer .
+                ?producer rdfs:label ?producerLabel .
+                FILTER(LANG(?producerLabel) = "en")
+                OPTIONAL {
+                    ?producer ^schema:about ?producerArticle .
+                    ?producerArticle schema:isPartOf <https://en.wikipedia.org/>.
+                }
+                OPTIONAL { ?producer wdt:P18 ?producerImage . }
+            }
+        }
+    }
+    """
+)
+
+DETAIL_SCREENWRITER_Q = prepareQuery(
+    DETAIL_NAMESPACES + """ 
+    SELECT * WHERE {
+        ?s v:hasWikidata ?item .
+        SERVICE <https://query.wikidata.org/sparql> {
+            OPTIONAL {
+                ?item wdt:P58 ?screenwriter .
+                ?screenwriter rdfs:label ?screenwriterLabel .
+                FILTER(LANG(?screenwriterLabel) = "en")
+                OPTIONAL {
+                    ?screenwriter ^schema:about ?screenwriterArticle .
+                    ?screenwriterArticle schema:isPartOf <https://en.wikipedia.org/>.
+                }
+                OPTIONAL { ?screenwriter wdt:P18 ?screenwriterImage . }
+            }
+        }
+    }
+    """
+)
+
+DETAIL_DIRECTOR_Q = prepareQuery(
+    DETAIL_NAMESPACES + """ 
+    SELECT * WHERE {
+        ?s v:hasWikidata ?item .
+        SERVICE <https://query.wikidata.org/sparql> {
+            OPTIONAL {
+                ?item wdt:P57 ?director .
+                ?director rdfs:label ?directorLabel .
+                FILTER(LANG(?directorLabel) = "en")
+                OPTIONAL {
+                    ?director ^schema:about ?directorArticle .
+                    ?directorArticle schema:isPartOf <https://en.wikipedia.org/>.
+                }
+                OPTIONAL { ?director wdt:P18 ?directorImage . }
+            }
+        }
+    }
+    """
+)
+
+DETAIL_CAST_Q = prepareQuery(
+    DETAIL_NAMESPACES + """ 
+    SELECT * WHERE {
+		?s v:hasWikidata ?item .
+		SERVICE <https://query.wikidata.org/sparql> {
+            OPTIONAL {
+                ?item p:P161 ?castStatement .
+                ?castStatement ps:P161 ?cast .
+                ?cast rdfs:label ?castLabel .
+                FILTER(LANG(?castLabel) = "en")
+                OPTIONAL {
+                    ?cast ^schema:about ?castArticle .
+                    ?castArticle schema:isPartOf <https://en.wikipedia.org/>.
+                }
+                OPTIONAL {
+                    ?castStatement pq:P453 ?castCharacter .
+                    ?castCharacter rdfs:label ?castCharacterLabel .
+                    FILTER(LANG(?castCharacterLabel) = "en")
+                }
+                OPTIONAL {
+                    ?item pq:P4633 ?castCharacterName .
+                }
+                OPTIONAL { ?cast wdt:P18 ?castImage . }
+            }
+		}
+	}"""
+)
 
 DETAIL_DBPEDIA_Q = prepareQuery(
     INITIAL_NAMESPACES + """
@@ -177,67 +290,90 @@ DETAIL_DBPEDIA_Q = prepareQuery(
 
 GROUPED_VARS = [
     ('country', 'countryLabel', 'countryArticle', 'countryArticleName'),
-    ('originalLanguage', 'originalLanguageLabel', 'originalLanguageArticle', 'originalLanguageArticleName')
+    ('originalLanguage', 'originalLanguageLabel', 'originalLanguageArticle', 'originalLanguageArticleName'),
+    ('director', 'directorLabel', 'directorArticle', 'directorArticleName', 'directorImage'),
+    ('screenwriter', 'screenwriterLabel', 'screenwriterArticle', 'screenwriterArticleName', 'screenwriterImage'),
+    ('producer', 'producerLabel', 'producerArticle', 'producerArticleName', 'producerImage'),
+    ('cast', 'castLabel', 'castArticle', 'castArticleName', 'castCharacterLabel', 'castCharacterName', 'castImage')
 ]
 
 GROUPED_VARS_FLAT = [var for group in GROUPED_VARS for var in group]
 
 def movie_detail(request, id):
-    query_result = local_g.query(DETAIL_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
-    
     result = None
     result_data = {}
-    
-    for var in query_result.vars:
-        var_str = str(var)
-        if var_str not in GROUPED_VARS_FLAT:
-            result_data[var_str] = []
-    
+
     for group_var in GROUPED_VARS:
         result_data[group_var[0]] = {}
 
-    for row in query_result:
-        if result is None:
-            result = row
-            
-        pending_value_group = {}
+    infobox_data = []
 
-        for index, value in enumerate(row):
-            value_str = str(value)
-            key = query_result.vars[index]
-            key_str = str(key)
+    # Querying from local database and Wikidata
+    # ────────────────────────────────────────
 
-            # find group_var that has key_str
-            group_var = next((group_var for group_var in GROUPED_VARS if key_str in group_var), None)
+    query_result = local_g.query(DETAIL_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
 
-            if group_var:
-                if group_var not in pending_value_group:
-                    pending_value_group[group_var] = {}
-                pending_value_group[group_var][key_str] = value_str
-            else:
-                if value_str not in result_data[key_str]:
-                    result_data[key_str].append(value_str)
+    initialize_result_data(result_data, query_result)
+    result = extract_and_group_results(result, result_data, query_result)
 
-        for group_var, group_value in pending_value_group.items():
-            group_var_key = group_var[0]
-            group_value_key = group_value[group_var_key]
+    print("Query 1 done.")
 
-            if group_var_key not in result_data:
-                result_data[group_var_key] = dict()
+    query_director_result = local_g.query(DETAIL_DIRECTOR_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
 
-            if group_value_key not in result_data[group_var_key]:
-                result_data[group_var_key][group_value_key] = group_value
+    initialize_result_data(result_data, query_director_result)
+    extract_and_group_results(result, result_data, query_director_result)
+
+    print("Query 2 done.")
+
+    query_screenwriter_result = local_g.query(DETAIL_SCREENWRITER_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
+
+    initialize_result_data(result_data, query_screenwriter_result)
+    extract_and_group_results(result, result_data, query_screenwriter_result)
+
+    print("Query 3 done.")
+
+    query_producer_result = local_g.query(DETAIL_PRODUCER_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
+
+    initialize_result_data(result_data, query_producer_result)
+    extract_and_group_results(result, result_data, query_producer_result)
+
+    print("Query 4 done.")
+    
+    query_cast_result = local_g.query(DETAIL_CAST_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
+
+    initialize_result_data(result_data, query_cast_result)
+    extract_and_group_results(result, result_data, query_cast_result)
+
+    print("Query 5 done.")
+
+    query_imdb_rating_result = local_g.query(DETAIL_IMDB_RATING_Q, initBindings={'s': rdflib.URIRef('http://example.com/data/' + id)})
+
+    for row in query_imdb_rating_result:
+        result_data['imdbRating'] = row.imdbRating
+        result_data['imdbVotes'] = row.imdbVotes
+
+    print("Query 6 done.")
+
+    # Querying from DBpedia
+    # ────────────────────────────────────────
 
     query_dbpedia_result = local_g.query(DETAIL_DBPEDIA_Q, initBindings={'article': rdflib.URIRef(result.article.replace('https', 'http'))})
-    print(rdflib.URIRef(result.article.replace('https', 'http')))
-    print(len(query_dbpedia_result))
 
     for row in query_dbpedia_result:
         result_data['abstract'] = row.abstract
 
+    print("Query DBpedia done.")
+
+    # Compiling obtained data
+    # ────────────────────────────────────────
+
+    # Release year and date
+
     release_year = result.releasedYear
     if result.releasedDate:
         release_year = datetime.datetime.strptime(result.releasedDate, "%Y-%m-%d").year
+
+    # Runtime
 
     runtime_minutes = int(result.runtime)
     runtime = {
@@ -247,7 +383,14 @@ def movie_detail(request, id):
         'text': f"{runtime_minutes // 60} h {runtime_minutes % 60} m"
     }
 
+    # Poster
+
     poster = re.sub(r'_U[XY]\d+.*?AL_', '_UX300_AL_', str(result.poster))
+    request_poster = requests.head(poster)
+    if request_poster.status_code != 200:
+        poster = None
+
+    # Abstract
 
     # wikipedia lead
     # wp_name = result_data['articleName'][0]
@@ -258,6 +401,11 @@ def movie_detail(request, id):
     # abstract_data = abstract_response.json()
     # abstract = abstract_data['query']['pages'][0]['extract'].split(' ==')[0]
     abstract = result_data['abstract']
+    # get first 5 sentences
+    if len(abstract.split('. ')) > 5:
+        abstract = '. '.join(abstract.split('. ')[:5]) + '.'
+
+    # Countries
 
     countries = []
 
@@ -268,6 +416,8 @@ def movie_detail(request, id):
             'url': country_data['countryArticle']
         })
 
+    # Languages
+
     languages = []
 
     for language in result_data['originalLanguage']:
@@ -277,28 +427,161 @@ def movie_detail(request, id):
             'url': language_data['originalLanguageArticle']
         })
 
-    infobox_data = [
+    # Director
+
+    directors = []
+
+    for director in result_data['director']:
+        director_data = result_data['director'][director]
+        directors.append({
+            'label': director_data['directorLabel'],
+            'url': director_data['directorArticle'],
+            'img': director_data['directorImage']
+        })
+        
+    # Screenwriter
+
+    screenwriters = []
+
+    for screenwriter in result_data['screenwriter']:
+        screenwriter_data = result_data['screenwriter'][screenwriter]
+        screenwriters.append({
+            'label': screenwriter_data['screenwriterLabel'],
+            'url': screenwriter_data['screenwriterArticle'],
+            'img': screenwriter_data['screenwriterImage']
+        })
+
+    # Producer
+
+    producers = []
+
+    for producer in result_data['producer']:
+        producer_data = result_data['producer'][producer]
+        producers.append({
+            'label': producer_data['producerLabel'],
+            'url': producer_data['producerArticle'],
+            'img': producer_data['producerImage'] if producer_data['producerImage'] and producer_data['producerImage'] != None else None
+        })
+
+    # Cast
+
+    cast = []
+
+    for cast_member in result_data['cast']:
+        cast_data = result_data['cast'][cast_member]
+        cast.append({
+            'label': cast_data['castLabel'],
+            'url': cast_data['castArticle'],
+            'role': cast_data['castCharacterLabel'] or cast_data['castCharacterName'],
+            'img': cast_data['castImage']
+        })
+
+    # Compiling infobox data
+    # ────────────────────────────────────────
+
+    cast_data = [
         {
-            'label': 'Release date',
-            'data': result.releasedDate or release_year
+            'label': 'Director',
+            'data': directors
         },
         {
-            'label': 'Runtime',
-            'data': runtime['text']
+            'label': 'Screenwriter',
+            'data': screenwriters
         },
         {
-            'label': 'Certification',
-            'data': result.certification
+            'label': 'Producer',
+            'data': producers
         },
         {
-            'label': 'Country',
-            'data': countries
-        },
-        {
-            'label': 'Language',
-            'data': languages
+            'label': 'Cast',
+            'data': cast
         }
     ]
+
+    if result.genre:
+        infobox_data.append({
+            'label': 'Genre',
+            'data': result.genre.split(',')
+        })
+
+    if directors:
+        infobox_data.append({
+            'label': 'Director',
+            'data': directors
+        })
+
+    if screenwriters:
+        infobox_data.append({
+            'label': 'Screenwriter',
+            'data': screenwriters
+        })
+
+    if producers:
+        infobox_data.append({
+            'label': 'Producer',
+            'data': producers
+        })
+
+    if result.releasedDate or release_year:
+        infobox_data.append({
+            'label': 'Release date',
+            'data': result.releasedDate or release_year
+        })
+
+    if runtime:
+        infobox_data.append({
+            'label': 'Runtime',
+            'data': f'{runtime["minutes"]} minutes'
+        })
+
+    if result.certification:
+        infobox_data.append({
+            'label': 'Certification',
+            'data': result.certification
+        })
+
+    if countries:
+        infobox_data.append({
+            'label': 'Country',
+            'data': countries
+        })
+
+    if languages:
+        infobox_data.append({
+            'label': 'Language',
+            'data': languages
+        })
+
+    if result.budget:
+        infobox_data.append({
+            'label': 'Budget',
+            'data': '$' + humanize.intword(result.budget)
+        })
+
+    if result.domesticOpening:
+        infobox_data.append({
+            'label': 'Domestic opening',
+            'data': '$' + humanize.intword(result.domesticOpening)
+        })
+
+    if result.domesticSales:
+        infobox_data.append({
+            'label': 'Domestic sales',
+            'data': '$' + humanize.intword(result.domesticSales)
+        })
+
+    if result.internationalSales:
+        infobox_data.append({
+            'label': 'International sales',
+            'data': '$' + humanize.intword(result.internationalSales)
+        })
+
+    if result.worldWideSales:
+        infobox_data.append({
+            'label': 'Worldwide sales',
+            'data': '$' + humanize.intword(result.worldWideSales)
+        })
+
 
     infobox_links = [
         {
@@ -340,13 +623,6 @@ def movie_detail(request, id):
             'url': f"https://www.rottentomatoes.com/{result.rottenTomatoesId}"
         })
 
-    print(result.overview)
-
-    # check if poster is available
-    request_poster = requests.head(poster)
-    if request_poster.status_code != 200:
-        poster = None
-
     context = {
         'movie_id': id,
         'movie_name': result.itemLabel or result.label,
@@ -356,10 +632,58 @@ def movie_detail(request, id):
         'poster': poster,
         'abstract': abstract,
         'overview': result.overview,
-
+        'rating': {
+            'imdb_rating': str(result_data.get('imdbRating', '')),
+            'imdb_votes': str(result_data.get('imdbVotes', '')),
+            'meta_score': str(result_data.get('metaScore', [''])[0])
+        },
         'infobox_data': infobox_data,
         'infobox_links': infobox_links,
+        'cast_data': cast_data,
         'result': result
     }
 
     return render(request, "movie-detail.html", context)
+
+def initialize_result_data(result_data, query_2_result):
+    for var in query_2_result.vars:
+        var_str = str(var)
+        if var_str in result_data:
+            continue
+        if var_str in GROUPED_VARS_FLAT:
+            continue
+        result_data[var_str] = []
+
+def extract_and_group_results(result, result_data, query_result):
+    for row in query_result:
+        if result is None:
+            result = row
+            
+        pending_value_group = {}
+
+        for index, value in enumerate(row):
+            value_str = str(value) if value else None
+            key = query_result.vars[index]
+            key_str = str(key)
+
+            # find group_var that has key_str
+            group_var = next((group_var for group_var in GROUPED_VARS if key_str in group_var), None)
+
+            if group_var:
+                if group_var not in pending_value_group:
+                    pending_value_group[group_var] = {}
+                pending_value_group[group_var][key_str] = value_str
+            else:
+                if value_str not in result_data[key_str]:
+                    result_data[key_str].append(value_str)
+
+        for group_var, group_value in pending_value_group.items():
+            group_var_key = group_var[0]
+            group_value_key = group_value[group_var_key]
+
+            if group_var_key not in result_data:
+                result_data[group_var_key] = dict()
+
+            if group_value_key not in result_data[group_var_key]:
+                result_data[group_var_key][group_value_key] = group_value
+    return result
